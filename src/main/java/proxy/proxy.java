61d87f2e74bca8ebbfa38386efe90af9f402198e
Bartlomiej Nawoj <thebalowen@gmail.com>
2019-05-26 18:46:09 +0200

    Printing the results from database is pretty now

--- src/main/java/proxy/proxy.java
+++ src/main/java/proxy/proxy.java
@@ -10,6 +10,7 @@ import redis.clients.jedis.Jedis;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -18,10 +19,11 @@ public class proxy {
         //Connecting to Redis server on localhost
         Jedis jedis = new Jedis("localhost");
         //Connecting to SQLite database
-        ConnectionSource connectionSource = new JdbcConnectionSource("jdbc:sqlite:E:/projekty/proxy_redis/chinook.db");
-        //Creating DAO for table tracks
+        String url = "jdbc:sqlite:E:/projekty/proxy_redis/chinook.db";
+        ConnectionSource connectionSource = new JdbcConnectionSource(url);
+        //Utworzenie data acces object dla tabeli customer
         Dao<customer,Integer> daoCustomer = DaoManager.createDao(connectionSource, customer.class);
-        //Creating HashMap for queries
+
         Gson gson = new Gson();
         while(true) {
 
@@ -30,7 +32,7 @@ public class proxy {
             System.out.println("Podaj query: ");
 
             String query = scan.nextLine();
-            query = query.toLowerCase();
+            String lCaseQuery = query.toLowerCase();
 
             if (query.equals("stop")) {
                 connectionSource.close();
@@ -39,14 +41,12 @@ public class proxy {
             else {
                 //Ustawienie klucza dla query
                 //klucz to query malymi literami - bez spacji
-                String redisKey = query.replaceAll(" ","");
-                //Checking if result of query exists in Redis
+                String redisKey = lCaseQuery.replaceAll(" ","");
+                //Sprawdzenie czy wyniki sa w Redis
                 if (jedis.exists(redisKey)) {
 
-                    //Getting result from Redis
                     String fromJedis = jedis.get(redisKey);
                     List<String[]> wynik = gson.fromJson(fromJedis, List.class);
-
 
                     //Printing result
                     for (int i = 0; i < wynik.size(); i++) {
@@ -54,16 +54,15 @@ public class proxy {
                     }
                     System.out.println("To byly wyniki z redisa");
                 } else {
-                    //Getting result from query
+                    //Pobranie wynikow
                     GenericRawResults<String[]> rawResults = daoCustomer.queryRaw(query);
                     List<String[]> result = rawResults.getResults();
 
-                    result.forEach(e -> {
-                        for (String s : e) {
-                            System.out.println(s);
-                        }
-                    });
+                    //Wyswietlenie wynikow
+                    result.forEach(arr ->
+                            System.out.println(Arrays.toString(arr)));
                     System.out.println("To byly wyniki z bazy");
+
                     //Serializing result of query to JSON
                     String queryRes = gson.toJson(result);
                     //Setting result in Redis
